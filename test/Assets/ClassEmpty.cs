d7e94de6dec79168e7757f598b57d24f1cb2f11c
FrancoDegno <francodegano@hotmail.com>
2018-01-09 23:52:45 -0300

    version Commit

--- test/Assets/ClassEmpty.cs
+++ test/Assets/ClassEmpty.cs
@@ -11,6 +11,6 @@ public class ClassEmpty : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        this.transform.position = Vector3.zero;
 	}
 }
--- test/Assets/print.cs
+++ test/Assets/print.cs
@@ -6,7 +6,7 @@ public class print : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        print("hola mundo");
+       
 	}
 	
 	// Update is called once per frame
