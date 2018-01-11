cd25e5228e8240a305b6134553d83a8cd05bd712
Franco <francodegano@hotmail.com>
2018-01-11 13:56:47 -0300

    commitNotebook

---
+++ test/Assets/Carrousel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carrousel : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform startObject;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		Vector3 nPos;
+		nPos = other.transform.position;
+		nPos.x = startObject.position.x;
+		other.transform.position = nPos;
+	}
+
+
+}
